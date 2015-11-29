a5b5ab2cd06fc23c420486ece392b067e8877798
Pauline Kelly <pauline.m.kelly1@gmail.com>
2015-11-29 15:01:28 +1300

    Include a formatter
    This may or may not be needed.

---
+++ formatter.js
@@ -0,0 +1,18 @@
+/*
+ Takes an input, finds out how best to format it, and returns the correct
+ output.
+*/
+const fastaExpr = new RegExp("\\[.*?\\]", "g");  //change this
+
+/* Helper methods go here - should be _private */
+
+module.exports = {
+
+  formatFASTA: (input) => {
+    return "Need to enter something here";
+  },
+
+  formatSingleString: (input) => {
+    return "Need to enter something here";
+  }
+}
