6fe24fe66b4261befd9473ef11f47995d87bfb75
Pauline Kelly <pauline.m.kelly1@gmail.com>
2015-11-29 15:00:58 +1300

    Propose handler for each branch

---
+++ algorithms/bioinformaticsStronghold/bioinfo-stronghold-handler.js
@@ -0,0 +1,18 @@
+let DNA = require("/lib");
+
+/*
+ Handles all of the problems in the Bioinformatics stronghold branch of
+ problems.
+ Imports the modules for each of these problems.
+*/
+
+/*
+ Locate the problem using the branch specified in the object.
+ Then find the objects ID, and call the function on it - they should all be
+ exported as modules.
+*/
+
+module.exports = {
+  //everything that you need to return in here.
+
+}
